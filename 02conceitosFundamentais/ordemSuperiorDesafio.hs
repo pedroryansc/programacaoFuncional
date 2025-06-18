f3606eef13cad0c18906010e7c22eae4e53e8bfc
pedroryansc <pedroryangamer@gmail.com>
2025-06-18 16:53:45 -0300

    Testes com exercícios da avaliação

--- 02conceitosFundamentais/ordemSuperiorDesafio.hs
+++ 02conceitosFundamentais/ordemSuperiorDesafio.hs
@@ -1,36 +1,33 @@
--- Função que verifica se uma idade é par 
-verificarPar :: Int -> Bool
-verificarPar idade = idade mod 2 == 0
+-- Função que verifica se a idade é par
+verificaPar :: Int -> Bool
+verificaPar idade = even idade
+
+-- Função que aplica "verificaPar"
+idadesPares :: [Int] -> [Int]
+idadesPares idades = filter verificaPar idades
 
 -- Função que dobra uma idade
 dobrar :: Int -> Int
 dobrar idade = idade * 2
 
+-- Função que aplica "dobrar"
+paresDobrados :: [Int] -> [Int]
+paresDobrados idades = map dobrar idades
+
 -- Função que soma as idades
 somar :: Int -> Int -> Int
 somar x y = x + y
+
+-- Função que soma as idades pares dobradas
+somaIdades :: [Int] -> Int
+somaIdades idades = foldl somar 0 idades
+
+-- Composição das funções
+executarOperacao = somaIdades . paresDobrados . idadesPares
 
 -- Lista original de idades
 idades :: [Int]
 idades = [20, 61, 59, 11, 82]
 
-{-
--- Lista de idades pares
-idadesPares :: [Int]
-idadesPares = filter verificarPar idades
-
--- Lista de idades pares dobradas
-idadesParesDobradas :: [Int]
-idadesParesDobradas = map dobrar idadesPares
-
--- Soma das idades pares dobradas
-somaIdades :: Int
-somaIdades = foldl somar 0 idadesParesDobradas
--}
-
--- Composição das funções
-somaIdades :: [Int] -> Int
-somaIdades = foldl somar 0 . map dobrar . filter verificarPar idades
-
 main = do
-    print somaIdades
+    print (executarOperacao idades)
